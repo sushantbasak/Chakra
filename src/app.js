69592a6ab1a27b5936017f8fb56a2fc4b512a838
sushantbasak <sushantbasak73@gmail.com>
2021-06-19 12:30:40 +0530

    Cleanup

--- src/app.js
+++ src/app.js
@@ -12,4 +12,5 @@ app.get('/health', (req, res) => {
   res.json({ status: 'UP' });
 });
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server started at port ${  port}`));
